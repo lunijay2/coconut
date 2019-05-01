4fc4be273bdba8c7ce4830b9ba66720f0d51cb3e
Ma Mingi <lunijay2@gmail.com>
2019-05-01 14:53:48 +0900

    ver190501.14:53

--- vue-coconut/src/components/HelloWorld.vue
+++ vue-coconut/src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
-    <ul>
+    <ul> 
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
