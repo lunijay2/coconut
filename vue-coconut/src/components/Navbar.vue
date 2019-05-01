3f75a11e765f3ae2121f1a63d782365a43bdb900
Ma Mingi <lunijay2@gmail.com>
2019-05-01 14:32:53 +0900

    ver190501.14:32

---
+++ vue-coconut/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">SM PAY</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link">모바일 결제 </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">마이 페이지 </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">상점 </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">상품 등록 </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">로그인 </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">회원가입</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">로그아웃</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link">인증서 발급 </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
