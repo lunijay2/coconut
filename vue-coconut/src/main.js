bbd7e76c16291a5081965016d93235d1719ca04b
Ma Mingi <lunijay2@gmail.com>
2019-05-05 17:44:56 +0900

    ver190505.17:45

--- vue-coconut/src/main.js
+++ vue-coconut/src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
-import VueFlashMessage from 'vue-flash-message';
+//import VueFlashMessage from 'vue-flash-message';
 
 Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
-Vue.use(VueFlashMessage);
+//Vue.use(VueFlashMessage);
 
 new Vue({
   render: h => h(App),
