b68a3ccbcab23356d2b1b21f7d1b81a57f3b9f55
JongHun Park2 <hoguro4124@naver.com>
2019-05-07 15:04:39 +0900

    ver190507

---
+++ vue-coconut/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <p>로그인</p>
+        <h1>ddddd</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
