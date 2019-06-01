c43a8c08b84c79d48e5c832750557794ea502775
lunijay2 <gksrnr8469@naver.com>
2019-06-01 16:45:07 +0900

    190531 Heroku v4

--- vue-coconut/src/components/Navbar.vue
+++ vue-coconut/src/components/Navbar.vue
@@ -5,7 +5,7 @@
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> 
             <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item" v-if="this.$store.state.pToken">
