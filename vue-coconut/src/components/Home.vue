4d6aad8b7af213c9d9af1841a9030fb39f80c8f0
khmo529 <43713981+khmo529@users.noreply.github.com>
2019-06-03 14:32:20 +0900

    Add files via upload

--- vue-coconut/src/components/Home.vue
+++ vue-coconut/src/components/Home.vue
@@ -1,25 +1,58 @@
 <template>
-    <div>
-        <h1>결제페이지 바로가기</h1><br>
-        <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
-    </div>
+    <div><h2><span style="background-color:#875B5B; background-size:50px 50px; border-radius: 10px;"><font color="#FFFFFF">상점</font></span></h2><br>
+      <router-link to="/CreateStore">        
+      <button type="submit" class="btn btn-dark">상품 등록하기</button>
+      </router-link>    
+<br><br>
+<div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#20c997"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">사진을 넣어주세요</text></svg>
+            <div class="card-body">
+              <p class="card-text">내용을 입력해주세요</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#d9534f"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">사진을 넣어주세요</text></svg>
+            <div class="card-body">
+              <p class="card-text">내용을 입력해주세요</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#17a2b8"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">사진을 넣어주세요</text></svg>
+            <div class="card-body">
+              <p class="card-text">내용을 입력해주세요</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+</div>
+</div>
 </template>
-
-<script>
-    import QrcodeVue from 'qrcode.vue';
-    export default {
-        name: "Home",
-        data() {
-            return {
-                value : 'https://coconutpay.herokuapp.com',
-                size : 120
-            }
-        },
-        components: {
-            QrcodeVue
-        }
-    }
-</script>
 
 <style scoped>
 
