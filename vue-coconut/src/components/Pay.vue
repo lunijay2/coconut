2441c45b3112a08c381300a5ab714e82c8576bb6
lunijay2 <gksrnr8469@naver.com>
2019-06-10 23:15:20 +0900

    190610 v2

--- vue-coconut/src/components/Pay.vue
+++ vue-coconut/src/components/Pay.vue
@@ -4,20 +4,19 @@
             <p class="error">{{ error }}</p>
             <!--<p class="decode-result">Last result: <b>{{ result }}</b></p>-->
             <qrcode-stream @decode="onDecode" @init="onInit" /><br><br>
-            <div class="list-group">
-                <a class="list-group-item list-group-item-action flex-column align-items-start">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h5 class="mb-1">{{result.order_no}}</h5>
-                        <small class="text-muted">{{result.product}}</small>
-                    </div>
-                    <p class="mb-1">{{product.description}}</p>
-                    <small class="text-muted">{{result.price}}원</small>
-                </a>
-                <br>
-            </div>
             <!--<button v-if="result" type="submit" class="btn btn-primary">결제</button>-->
         </form>
-
+        <div class="list-group">
+            <a class="list-group-item list-group-item-action flex-column align-items-start">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{result.order_no}}</h5>
+                    <small class="text-muted">{{result.product}}</small>
+                </div>
+                <p class="mb-1">{{product.description}}</p>
+                <small class="text-muted">{{result.price}}원</small>
+            </a>
+            <br>
+        </div>
     </div>
 </template>
 
