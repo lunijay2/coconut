16b1cbea2fb4e3860aec7679e0bfc3a4c076b253
lunijay2 <gksrnr8469@naver.com>
2019-06-10 20:22:51 +0900

    190610 v2

--- config/database.js
+++ config/database.js
@@ -10,11 +10,12 @@ module.exports = {
 }*/
 
 module.exports = {
-    host     : 'db4free.net',
-    user     : 'coconut',
+    host : 'db4free.net',
+    user : 'coconut',
     password : 'jbis2019',
-    port     : 3306,
+    port : 3306,
     database : 'coconut',
-    secret: 'mysupersecret',
-    connectionLimit : 100
+    secret : 'mysupersecret',
+    connectionLimit : 100,
+    timeout : 60 * 60 * 1000
 }
