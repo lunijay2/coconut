14e98a75a854834df57ca5483ff16d33e1e69139
lunijay2 <gksrnr8469@naver.com>
2019-09-22 22:21:58 +0900

    190922v1

--- vue-coconut/src/components/Navbar.vue
+++ vue-coconut/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <!--홈 <이미지-->
         <div class="container">
-        <router-link to="/" class="navbar-brand floa">
+        <router-link to="/" class="navbar-brand floa jb-larger">
             COCONUT
             <!--<div class="centered">COCONUT</div>-->
         </router-link>
@@ -21,21 +21,23 @@
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item" v-if="this.$store.state.pToken">
-                    <router-link to="/MyPage" class="nav-link">마이페이지</router-link>
+                        <router-link to="/MyPage" class="nav-link jb-default-3">마이페이지</router-link>
                 </li>
                 <li class="nav-item" v-if="this.$store.state.pToken">
-                    <router-link to="/Cert" class="nav-link">Master Cert</router-link>
+                        <router-link to="/Cert" class="nav-link jb-default-3">인증서 발급</router-link>
                 </li>
+                <!--
                 <li class="nav-item" v-if="this.$store.state.pToken">
-                    <router-link to="/CertTest" class="nav-link">Cert Test</router-link>
+                    <router-link to="/CertTest" class="nav-link jb-default-3">Cert Test</router-link>
                 </li>
+                -->
                 <!--
                 <li class="nav-item" v-if="this.$store.state.pToken">
                     <router-link to="/AdditionalCert" class="nav-link">Additional Cert</router-link>
                 </li>
                 -->
                 <li class="nav-item">
-                    <router-link to="/AllProduct" class="nav-link">모든 상품</router-link>
+                        <router-link to="/AllProduct" class="nav-link jb-default-3">모든 상품</router-link>
                 </li>
                 <!--
                 <li class="nav-item">
@@ -43,13 +45,13 @@
                 </li>
                 -->
                 <li class="nav-item">
-                    <router-link to="/Pay" class="nav-link">QR결제</router-link>
+                    <router-link to="/Pay" class="nav-link jb-default-3">QR결제</router-link>
                 </li>
                 <li class="nav-item" v-if="!this.$store.state.pToken">
-                    <router-link to="/ChoiceMemberType" class="nav-link">회원가입</router-link>
+                    <router-link to="/ChoiceMemberType" class="nav-link jb-default-3">회원가입</router-link>
                 </li>
                 <li class="nav-item" v-if="!this.$store.state.pToken">
-                    <router-link to="/Login" class="nav-link">로그인</router-link>
+                    <router-link to="/Login" class="nav-link jb-default-3">로그인</router-link>
                 </li>
                 <!--
                 <li class="nav-item">
@@ -60,7 +62,7 @@
                 </li>
                 -->
                 <li class="nav-item" v-if="this.$store.state.pToken">
-                    <a class="nav-link" href="#" @click.prevent="onLogoutClick">로그아웃</a>
+                    <a class="nav-link jb-default-3" href="#" @click.prevent="onLogoutClick">로그아웃</a>
                 </li>
             </ul>
         </div>
@@ -86,4 +88,9 @@
         text-decoration: none;
         color: #333333;
     }
+    .jb-default-1 { font-size: 16px; }
+    .jb-default-2 { font-size: 22px; }
+    .jb-default-3 { font-size: 25px; }
+    .jb-smaller { font-size: smaller; }
+    .jb-larger { font-size: 40px; }
 </style>
